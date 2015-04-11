e0f52185312c8fce6abbb7941d66fdbd93974c47
Brendan Batliner <brendan.batliner@gmail.com>
2015-04-11 10:44:03 -0500

    Setting up for Heroku

---
+++ config/mongoose.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI, function(err) {
+	if (err) {
+		console.log('MongoDB connection error: ' + err);
+	}
+});
+
+var db = mongoose.connection;
+db.once('open', function (data) {
+	console.log("MongoDB connection successful.");
+});
+
+module.exports = db;
